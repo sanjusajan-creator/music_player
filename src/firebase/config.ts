54e36f5faf88560e456c27b175b5a3cf3e54cc1e
SANJU SAJAN <sanjusajan9512@gmail.com>
2026-04-22 05:21:40 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5324937127-28b40",
+  "appId": "1:578250216810:web:13416b9e26eac39d77c113",
+  "apiKey": "AIzaSyAqRj-dFqFr-9-RGzkfjADDt4KEWqtWHzA",
+  "authDomain": "studio-5324937127-28b40.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "578250216810"
+};
